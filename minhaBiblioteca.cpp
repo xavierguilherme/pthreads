77e6561bd9663cc6cb1893a5e71e7d1763e7b063
Alejandrodsp <pereira_randro@hotmail.com>
2021-05-18 18:56:20 -0300

    c++

--- minhaBiblioteca.cpp
+++ minhaBiblioteca.cpp
@@ -1,6 +1,11 @@
 #include <pthread.h>
+#include <iostream>
+#include <list>
+#include <iterator>
 #include <malloc.h>
 #include "minhaBiblioteca.h"
+
+using namespace std;
 
 typedef struct Trabalho
 {
@@ -8,55 +13,42 @@ typedef struct Trabalho
     void *(*func)(void *); // Função a ser executada
     void *dta;             // Parâmetros de f
     void *res;             // Retorno de f
-    struct Trabalho *prev, *next;
 } Trabalho;
 
-typedef struct Pos
-{
-    Trabalho *head;
-    Trabalho *tail;
-} Pos;
-
-Trabalho *listaTrabalhos, *listaResultados;
-Pos *posTrabalhos, *posResultados; // Posições nas listas
+list<Trabalho> listaTrabalhos, listaResultados;
+sem_t plivre, pocupada;
+pthread_mutex_t m_Trabalhos, m_Resultados;
 
 static pthread_t *pvs; // Processadores virtuais
 int fim;               //Variavel para indicar o fim do programa
 int nPvs;              //Variavel que vai conter o numero m de processadores virtuais, para conseguir dar join neles na função finish
 int ids = 0;           //Variavel para controlar os IDs dos trabalhos
-
 Trabalho *pegaTrabalho()
 {
-    Trabalho *trab = malloc(sizeof(Trabalho));
-    if (listaTrabalhos == NULL)
-        return NULL;
+    Trabalho *trab;
 
-    trab = posTrabalhos->head;
-    posTrabalhos->head = posTrabalhos->head->next;
-    if (posTrabalhos->head == NULL)
-        return trab;
+    // if (listaTrabalhos.empty())
+    // return NULL;
 
-    posTrabalhos->head->prev = NULL;
-    trab->next = NULL;
+    sem_wait(&pocupada);
+    pthread_mutex_lock(&m_Trabalhos);
+    trab = listaTrabalhos.front();
+    listaTrabalhos.pop_front();
+    pthread_mutex_unlock(&m_Trabalhos);
+    sem_post(&plivre);
+    sleep(1);
     return trab;
 }
 
 void guardaResultado(Trabalho *trab, void *res)
 {
+    sem_wait(&plivre);
+    pthread_mutex_lock(&m_Resultados);
     trab->res = res;
-    if (listaResultados == NULL)
-    {
-        trab = malloc(sizeof(Trabalho));
-        posResultados->head = trab;
-        posResultados->head->prev = NULL;
-        posResultados->head->next = NULL;
-    }
-    else
-    {
-        posResultados->tail->next = trab;
-        trab->prev = posResultados->tail;
-        posResultados->tail = trab;
-    }
+    listaResultados.push_back(trab);
+    pthread_mutex_unlock(&m_Resultados);
+    sem_post(&pocupada);
+    sleep(1);
 }
 
 void *criaPv(void *dta)
@@ -64,7 +56,7 @@ void *criaPv(void *dta)
     void *res;
     Trabalho *trab;
 
-    while (listaTrabalhos != NULL)
+    while (!listaTrabalhos.empty())
     {
         trab = pegaTrabalho();
         res = trab->func(trab->dta);
@@ -77,7 +69,9 @@ void *criaPv(void *dta)
 int start(int m)
 {
     int retorno = 0;
-
+    pthread_mutex_init(&m, NULL);
+    sem_init(&plivre, 0, m);
+    sem_init(&pocupada, 0, 0);
     fim = 0;
     nPvs = m;
     pvs = (pthread_t *)malloc(m * sizeof(pthread_t));
@@ -99,31 +93,34 @@ int spawn(struct Atrib *atrib, void *(*t)(void *), void *dta)
 {
     Trabalho *trab;
 
-    trab = malloc(sizeof(Trabalho));
+    trab = (Trabalho *)malloc(sizeof(Trabalho));
     if (trab == NULL)
     {
         return 0;
     }
-    else
-    {
-        ids++;
-        trab->tId = ids;
-        trab->func = t;
-        trab->dta = dta;
+    sem_wait(&plivre);
+    pthread_mutex_lock(&m_Trabalhos);
+    ids++;
+    trab->tId = ids;
+    trab->func = t;
+    trab->dta = dta;
+    listaTrabalhos.push_back(trab);
+    pthread_mutex_unlock(&m_Trabalhos);
+    sem_post(&pocupada);
+    sleep(1);
+    return ids;
+}
+int sync(int tId, void **res)
+{
+    
 
-        if (listaTrabalhos == NULL)
-        {
-            posTrabalhos->head = trab;
-            posTrabalhos->head->prev = NULL;
-            posTrabalhos->head->next = NULL;
-        }
-        else
-        {
-            posTrabalhos->tail->next = trab;
-            trab->prev = posTrabalhos->tail;
-            posTrabalhos->tail = trab;
-        }
-
-        return ids;
+    sem_wait(&pocupada);
+    pthread_mutex_lock(&m_Resultados);
+     list <Trabalho> :: iterator it;
+    for(it = listaTrabalhos.begin(); it != listaTrabalhos.end(); ++it){
+        
     }
+    pthread_mutex_unlock(&m_Resultados);
+    sem_post(&plivre);
+    sleep(1);
 }
