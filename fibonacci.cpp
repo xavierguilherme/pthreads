7d50980228acff496c8a5dd0db23142137710e26
xavierguilherme <xaviergx99@gmail.com>
2021-05-20 15:17:24 -0300

    Implementação spawn e sync

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -5,6 +5,7 @@
 
 void *fibo(void* dta){
     int *n = (int*) dta;
+    printf("            N = %d\n", *n);
     int *n1, *n2, *r = (int *) malloc(sizeof(int)),
             *r1, *r2, t1, t2;
     struct Atrib a1, a2;
@@ -12,11 +13,11 @@ void *fibo(void* dta){
     if (*n <= 2) *r = 1;
     else{
         n1 = (int *) malloc(sizeof(int));
-        *n1 = *n - 1;
+        *n1 = (*n) - 1;
         a1.p = 0; a1.c = *n1;
         t1 = spawn(&a1, fibo, (void*) n1);
         n2 = (int *) malloc (sizeof(int));
-        *n2 = *n - 2;
+        *n2 = (*n) - 2;
         a2.p = 0; a1.c = *n2;
         t2 = spawn(&a2, fibo, (void*) n2);
         sync(t1, (void**) &r1);
@@ -32,9 +33,9 @@ int main(){
     int n, *r, tId;
     struct Atrib a;
 
-    start (4);
+    start(4);
 
-    n = 10;
+    n = 4;
     a.p = 0; a.c = n;
     tId = spawn (&a, fibo, &n);
     sync(tId, (void**)&r);
--- minhaBiblioteca.cpp
+++ minhaBiblioteca.cpp
@@ -16,16 +16,18 @@ typedef struct Trabalho
     void *res;             // Retorno de f
 } Trabalho;
 
-list<Trabalho> listaTrabalhos, listaResultados;
-sem_t plivre, pocupada;
+list<Trabalho*> listaTrabalhos, listaResultados;
+sem_t plivre, pocupada, rlivre, rocupada;
 pthread_mutex_t m_Trabalhos, m_Resultados;
 
 static pthread_t *pvs; // Processadores virtuais
-int fim;               //Variavel para indicar o fim do programa
+int fim = 0;               //Variavel para indicar o fim do programa
 int nPvs;              //Variavel que vai conter o numero m de processadores virtuais, para conseguir dar join neles na função finish
 int ids = 0;           //Variavel para controlar os IDs dos trabalhos
+
 Trabalho *pegaTrabalho()
 {
+    printf("PEGA TRABALHO\n");
     Trabalho *trab;
 
      //if (listaTrabalhos.empty())
@@ -33,63 +35,81 @@ Trabalho *pegaTrabalho()
 
     sem_wait(&pocupada);
     pthread_mutex_lock(&m_Trabalhos);
-    trab = &listaTrabalhos.front();
-    printf("%d ", *(int*)trab->dta);
-    //listaTrabalhos.pop_front();
+    printf("-- DENTRO DE PEGA TRABALHO\n");
+    trab = listaTrabalhos.front();
+    printf("ID DE PEGA TRABALHO = %d\n", trab->tId);
+    listaTrabalhos.pop_front();
     pthread_mutex_unlock(&m_Trabalhos);
     sem_post(&plivre);
-    //sleep(1);
-    
+    sleep(1);
+
     return trab;
 }
 
 void guardaResultado(Trabalho* trab, void *res)
 {
-
-    sem_wait(&plivre);
+    printf("GUARDA RESULTADO    %d\n", *(int*)res);
+    sem_wait(&rlivre);
     pthread_mutex_lock(&m_Resultados);
+    printf("-- DENTRO DE GUARDA RESULTADO\n");
     trab->res = res;
-    listaResultados.push_back(*trab);
+    listaResultados.push_back(trab);
     pthread_mutex_unlock(&m_Resultados);
-    sem_post(&pocupada);
+    sem_post(&rocupada);
     sleep(1);
 }
 
 void *criaPv(void *dta)
 {
+    printf("CRIA PV\n");
     void *res;
     Trabalho *trab;
     while(!fim){
-    if (!listaTrabalhos.empty())
-    {
-        trab = pegaTrabalho();
-        res = trab->func(trab->dta); 
-        guardaResultado(trab, res);
-    }
+        if (!listaTrabalhos.empty())
+        {
+            trab = pegaTrabalho();
+            printf("            DTA = %d\n", *(int*)trab->dta);
+            res = trab->func(trab->dta);
+            guardaResultado(trab, res);
         }
+    }
     return NULL;
 }
 
 int start(int m)
 {
+    printf("START\n");
     int retorno = 0;
     pthread_mutex_init(&m_Trabalhos, NULL);
     pthread_mutex_init(&m_Resultados, NULL);
     sem_init(&plivre, 0, m);
     sem_init(&pocupada, 0, 0);
-    fim = 0;
+    sem_init(&rlivre, 0, m);
+    sem_init(&rocupada, 0, 0);
+
     nPvs = m;
+
     pvs = (pthread_t *)malloc(m * sizeof(pthread_t));
 
     for (int i = 0; i < nPvs; i++)
         retorno = pthread_create(&pvs[i], NULL, criaPv, NULL);
+
+    sleep(1);
 
     return retorno;
 }
 
 void finish()
 {
+    printf("FINISH\n");
     fim = 1;
+
+    pthread_mutex_destroy(&m_Trabalhos);
+    pthread_mutex_destroy(&m_Resultados);
+    sem_destroy(&plivre);
+    sem_destroy(&pocupada);
+    sem_destroy(&rlivre);
+    sem_destroy(&rocupada);
     
     for (int i = 0; i < nPvs; i++)
         pthread_join(pvs[i], NULL);
@@ -97,86 +117,85 @@ void finish()
 
 int spawn(struct Atrib *atrib, void *(*t)(void *), void *dta)
 {
+    printf("SPAWN   DTA: %d\n", *(int*)dta);
     Trabalho *trab;
     trab = (Trabalho *)malloc(sizeof(Trabalho));
-    if (trab == NULL)
-    {
+    if (trab == NULL){
         return 0;
     }
+
+    /* int value;
+    sem_getvalue(&plivre, &value);
+    printf("SEM: %d\n", value); */
+    
+
     sem_wait(&plivre);
     pthread_mutex_lock(&m_Trabalhos);
+    printf("-- DENTRO DE SPAWN\n");
     ids++;
     trab->tId = ids;
     trab->func = t;
     trab->dta = dta;
-    listaTrabalhos.push_back(*trab);
+    listaTrabalhos.push_back(trab);
+    printf("ID TRAB SPAWNADO = %d\n", trab->tId);
     pthread_mutex_unlock(&m_Trabalhos);
     sem_post(&pocupada);
     
     return ids;
 }
 
+Trabalho* pegaTrabalhoPorId(int tId, list<Trabalho*> lista) {
+    list <Trabalho*> :: iterator it;
+
+    printf("TAMANHO DA LISTA = %d\n", lista.size());
+
+    Trabalho* trab;
+
+    for(it = lista.begin(); it != lista.end(); it++) {
+        printf("ID -> %d    TRABALHO -> %d\n", (*it)->tId, *(int*)((*it)->dta));
+        if ((*it)->tId == tId) {
+            trab = *it;
+            lista.erase(it);
+            break;
+        }
+    }
+
+    return trab;
+}
+
 int sync(int tId, void **res)
 {
+    printf("SYNC    ID: %d\n", tId);
+    Trabalho* trab;
 
-    int flag = 0;
-
-    sem_wait(&pocupada);
+    sem_wait(&rocupada);
     pthread_mutex_lock(&m_Resultados);
-    list <Trabalho> :: iterator it;
-    // Caso 1
-    /*
-     * a tarefa está na lista de tarefas prontas. Neste caso, a execução da primitiva
-     * sync deve retirar a tarefa da lista de tarefas prontas e executá-la,
-     * retornando diretamente o resultado, sem a necessidade de incluir a tarefa concluída na lista de tarefas finalizada.
-     */
-    for(it = listaTrabalhos.begin(); it != listaTrabalhos.end(); ++it) {
-        if (it->tId == tId) {
-            res = (void **)(it->func(it->dta));
-            listaTrabalhos.erase(it);
-            flag = 1;
-            break;
-        }
-    }
-
-    // Caso 2
-    /*
-     * a tarefa está na lista de tarefas terminadas. Neste caso,
-     * basta retirar a tarefa da lista de tarefas terminadas e recuperar o dado de retorno.
-     */
-    for(it = listaResultados.begin(); it != listaResultados.end(); ++it) {
-        if (it->tId == tId) {
-            res = (void **)(it->res);
-            listaResultados.erase(it);
-            flag = 1;
-            break;
-        }
-    }
-
-    // Caso 3
-    /*
-     * a tarefa está em execução (não está em nenhuma das listas). Neste caso, a tarefa está sendo executada por um outro pv
-     * e a implementação deve solucionar o problema. Possíveis soluções: o pv entra em estado de sleep durante um período de
-     * tempo e volta a procurar a tarefa na lista de tarefas finalizadas, apostando que o tempo de espera é curto; o pv retira
-     * uma nova tarefa da lista de prontos e inicia sua execução, na expectativa de que quando esta nova tarefa terminar,
-     * a tarefa a ser sincronizada terá terminado.
-     */
-    if (!flag) {
-        for(int i = 0; i < 10; i++) {
+    printf("-- DENTRO DE SYNC\n");
+    // CASO 1
+    printf("LISTA TRABALHOS\n");
+    trab = pegaTrabalhoPorId(tId, listaTrabalhos);
+    if (trab == NULL) {
+        // CASO 2
+        printf("LISTA RESULTADOS\n");
+        trab = pegaTrabalhoPorId(tId, listaResultados);
+        if (trab == NULL) {
             sleep(1);
-            for (it = listaResultados.begin(); it != listaResultados.end(); ++it) {
-                if (it->tId == tId) {
-                    res = (void **) (it->res);
-                    listaResultados.erase(it);
-                    flag = 1;
-                    break;
-                }
-            }
+            // CASO 3
+            trab = pegaTrabalhoPorId(tId, listaResultados);
+            if (trab == NULL)
+                return 0;
+            else
+                res = (void **)(trab->res);
+        } else {
+            res = (void **)(trab->res);
         }
+    } else {
+        res = (void **)(trab->func(trab->dta));
     }
+    printf("SYNC    RES: %d\n", *(int*)res);
     pthread_mutex_unlock(&m_Resultados);
-    sem_post(&plivre);
+    sem_post(&rlivre);
     sleep(1);
 
-    return flag;
+    return 1;
 }
